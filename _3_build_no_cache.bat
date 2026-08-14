36b68e7b1f6186838d969cd123f6dd8b996f5059
HW <ruslan.s.burkov@gmail.com>
2026-08-14 19:11:31 +0300

    #Widler-3: compose разделен на несколько файлов

--- _3_build_no_cache.bat
+++ _3_build_no_cache.bat
@@ -1,5 +1,8 @@
 @echo off
 echo Building without cache...
-docker compose build --no-cache
+docker compose ^
+  -f compose.yml ^
+  -f compose.app.yml ^
+  -f compose.observability.yml build --no-cache
 echo Done.
 pause
--- _7_start.bat
+++ _7_start.bat
@@ -3,6 +3,6 @@ echo Up...
 docker compose ^
   -f compose.yml ^
   -f compose.app.yml ^
-  -f compose.observability.yml up
+  -f compose.observability.yml start
 echo Done.
 pause
